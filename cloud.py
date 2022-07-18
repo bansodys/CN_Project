c02332dae478254086634167262a2026adcccd87
YASH <bansodys@rknec.edu>
2022-07-18 13:28:16 +0530

    added codes

--- cloud.py
+++ cloud.py
@@ -5,7 +5,7 @@ fl=os.listdir()
 # print(fl)
 os.system("git add -A")
 os.system('''git commit -m "added codes" ''')
-os.system('''git remote add origin https://github.com/bansodys/CN_Project.git''')
+# os.system('''git remote add origin https://github.com/bansodys/CN_Project.git''')
 os.system('''git push -u origin master''')
 
 
